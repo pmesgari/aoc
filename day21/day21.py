8fcb544a18a92d4515e12f33f85396d362aaa713
Payam Mesgari <payam.mesgari@gmail.com>
2022-01-02 21:46:59 +0100

    day21

--- day21/day21.py
+++ day21/day21.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from dataclasses import dataclass
 
 
 class Dice:
@@ -26,9 +27,56 @@ class Dice:
         return total
 
 
+q_dice_permutations = {}
+for i in range(1, 4):
+    for j in range(1, 4):
+        for k in range(1, 4):
+            q_dice_permutations.update({i +j + k: q_dice_permutations.get(i +j + k, 0) + 1})
+
+print(q_dice_permutations)
+
+
+
 class Player(Enum):
     White = 'white'
     Black = 'black'
+
+
+@dataclass(frozen=True)
+class GameState:
+    player: Player
+    score: int
+    pos: int
+
+import functools
+
+@functools.lru_cache(maxsize=None)
+def quantum_play(white_state, black_state):
+    # we assume black just played
+    if black_state.score >= 21:
+        return [0, 1]
+    else:
+        white_wins = 0
+        black_wins = 0
+        for key, value in q_dice_permutations.items():
+            mod = (key + white_state.pos) % 10
+            if mod > 0:
+                next_pos = mod
+            else:
+                next_pos = 10
+            next_score = white_state.score + next_pos
+            black_win, white_win = quantum_play(black_state, GameState(player=white_state.player, score=next_score, pos=next_pos)) 
+            black_wins += black_win * value
+            white_wins += white_win * value
+    return (white_wins, black_wins)
+
+
+white = GameState(player=Player.White, score=0, pos=2)
+black = GameState(player=Player.Black, score=0, pos=7)
+
+win1, win2 = quantum_play(white, black)
+print(win1, win2)
+print(max(win1, win2))
 
 
 class Board:
@@ -67,11 +115,11 @@ class Board:
         return Player.Black if player == Player.White else Player.White
 
 
-board = Board()
+# board = Board()
 # board.take_turn()
 # print(board.scores, board.pos)
-while not board.is_win:
-    board.take_turn()
+# while not board.is_win:
+#     board.take_turn()
 
 # for _ in range(400):
 #     board.take_turn()
